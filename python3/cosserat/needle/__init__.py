a122b8e216386affe217d18b8cddf7c5fb1be573
Younes <yinoussa.adagolodjo@gmail.com>
2021-03-02 15:41:22 +0100

    last modif according to python3

---
+++ python3/cosserat/needle/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-longeurS
+"""
+Template for needle.
+
+Content:
+********
+.. 	scenes related to the use of cosserat model needle  
+..	in a direct simulation 
+
+   
+"""
+#from needle import cosseratNeedle
