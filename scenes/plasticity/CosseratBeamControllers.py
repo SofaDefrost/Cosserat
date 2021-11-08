dda39a3cf9df9999bee56da212c89857940e813b
Camille <camille.krewcun@inria.fr>
2021-11-08 10:47:04 +0100

    Add interactive controller for catheter insertion
    The controller allows to apply a rotation on a Cosserat beam, on a given
    frame (among the mapped frames). The insertion is also controlled
    manually, with a process changing dynamically the frame on which is
    applied the control, while the catheter is moving. The purpose of this
    functionnality is to mimick the physician's gesture when inserting a
    catheter in a vessel.

--- scenes/plasticity/CosseratBeamControllers.py
+++ scenes/plasticity/CosseratBeamControllers.py
@@ -12,11 +12,15 @@ Created on Thu Oct 14 2021
 """
 
 import Sofa
+import Sofa.constants.Key as Key
 import numpy as np
+import math
+from pyquaternion import Quaternion as Quat
+
 
 class InsertionController(Sofa.Core.Controller):
 
-    def __init__(self, rootNode, insertionRate, *args, **kwargs):
+    def __init__(self, rootNode, insertionRate, insertionDirection, *args, **kwargs):
         # These are needed (and the normal way to override from a python class)
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
 
@@ -25,33 +29,33 @@ class InsertionController(Sofa.Core.Controller):
         cosseratBeamNode = rootNode.getChild('cosseratBeamNode')
         self.rigidBaseNode = cosseratBeamNode.getChild('rigidBaseNode')
         self.insertionRate = insertionRate
+        self.insertionDirection = np.array(insertionDirection / np.linalg.norm(insertionDirection))  # normalised
         self.totalTime = 0.0
         self.nbIterations = 0
         self.autoInsertion = True
         self.backwards = False
 
-    def onAnimateBeginEvent(self, event): # called at each begin of animation step
+        # constructs a grid of indices to access only position DoFs of the rigid particle
+        self.posDoFsIdGrid = np.ix_([0], [0, 1, 2])
+
+    def onAnimateBeginEvent(self, event):  # called at each begin of animation step
         if self.autoInsertion:
             with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
-                if (not self.backwards):
-                    controlPointPos[0][0] += self.insertionRate[0]
-                    controlPointPos[0][1] += self.insertionRate[1]
-                    controlPointPos[0][2] += self.insertionRate[2]
+                if not self.backwards:
+                    controlPointPos[self.posDoFsIdGrid] += self.insertionDirection * self.insertionRate
                 else:
-                    controlPointPos[0][0] -= self.insertionRate[0]
-                    controlPointPos[0][1] -= self.insertionRate[1]
-                    controlPointPos[0][2] -= self.insertionRate[2]
+                    controlPointPos[self.posDoFsIdGrid] -= self.insertionDirection * self.insertionRate
 
         self.totalTime = self.totalTime + self.rootNode.findData('dt').value
         self.nbIterations = self.nbIterations + 1
 
-    def onAnimateEndEvent(self, event): # called at each end of animation step
+    def onAnimateEndEvent(self, event):  # called at each end of animation step
         pass
 
     def onKeypressedEvent(self, event):
         # Pressing A key activates/deactivates the automatic insertion (in the X axis direction)
-        if event['key']=='A':
-            if (self.autoInsertion):
+        if event['key'] == 'A':
+            if self.autoInsertion:
                 print("Automatic insertion stopped")
             else:
                 print("Automatic insertion started")
@@ -59,7 +63,7 @@ class InsertionController(Sofa.Core.Controller):
 
         # Pressing R key inverse the insertion (switch between pushing and pulling)
         if event['key'] == 'C':
-            if (self.backwards):
+            if self.backwards:
                 print("Now pushing the catheter")
             else:
                 print("Now pulling the catheter")
@@ -68,7 +72,8 @@ class InsertionController(Sofa.Core.Controller):
 
 class InteractiveInsertionController(Sofa.Core.Controller):
 
-    def __init__(self, rootNode, initFrame, *args, **kwargs):
+    def __init__(self, rootNode, initFrameId, insertionRate, insertionDirection, switchFrameDistance,
+                 incrementAngle, *args, **kwargs):
         # These are needed (and the normal way to override from a python class)
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
 
@@ -76,35 +81,63 @@ class InteractiveInsertionController(Sofa.Core.Controller):
         self.controlPointNode = rootNode.getChild('controlPointNode')
         cosseratBeamNode = rootNode.getChild('cosseratBeamNode')
         self.rigidBaseNode = cosseratBeamNode.getChild('rigidBaseNode')
-        self.insertionRate = 25
+        self.mappedFramesNode = self.rigidBaseNode.getChild('MappedFrames')
+
+        self.insertionRate = insertionRate
+        self.insertionDirection = np.array(insertionDirection / np.linalg.norm(insertionDirection))  # normalised
+        self.currentFrameId = initFrameId
+        self.switchFrameDistance = switchFrameDistance
+        self.incrementAngle = incrementAngle
+
+        # Computing the incremental quaternions for rotation
+        # Taking the direction of insertion as rotation axis
+        qw = math.cos(math.radians(self.incrementAngle)/2)
+        plusQuat = self.insertionDirection * math.sin(math.radians(self.incrementAngle)/2)
+        minusQuat = -plusQuat
+        self.plusQuat = Quat(np.insert(plusQuat, 0, qw))
+        self.minusQuat = Quat(np.insert(minusQuat, 0, qw))
+
         self.totalTime = 0.0
         self.nbIterations = 0
-        self.autoInsertion = True
+        self.nbFrames = len(self.mappedFramesNode.FramesMO.position)
+        self.autoInsertion = False
         self.backwards = False
 
-    # Default Events *********************************************
-    def onAnimateBeginEvent(self, event): # called at each begin of animation step
+        self.controlPointRefPos = np.array(self.mappedFramesNode.FramesMO.position.value[self.currentFrameId])  # copy
+
+        # constructs a grid of indices to access only position DoFs of the rigid particle
+        self.posDoFsIdGrid = np.ix_([0], [0, 1, 2])
+        self.posDoFsIdGridSingle = np.ix_([0, 1, 2])
+        # constructs a grid of indices to access only orientation DoFs of the rigid particle
+        self.quatDoFsIdGrid = np.ix_([0], [3, 4, 5, 6])
+
+        # Placing the control rigid particle on the first frame to 'push'
+        with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+            with self.mappedFramesNode.FramesMO.position.writeable() as currentFramePos:
+                controlPointPos[0] = currentFramePos[self.currentFrameId]
+                print("Control point moved to new position : {}".format(controlPointPos[0]))
+        # Attaching the RestShapeSpringsForceField to this frame
+        with self.mappedFramesNode.controlSpring.points.writeable() as controlSpringPoints:
+            controlSpringPoints = self.currentFrameId
+
+    def onAnimateBeginEvent(self, event):  # called at each begin of animation step
         if self.autoInsertion:
             with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
-                if (not self.backwards):
-                    controlPointPos[0][0] += self.insertionRate[0]
-                    controlPointPos[0][1] += self.insertionRate[1]
-                    controlPointPos[0][2] += self.insertionRate[2]
+                if not self.backwards:
+                    controlPointPos[self.posDoFsIdGrid] += self.insertionDirection * self.insertionRate
                 else:
-                    controlPointPos[0][0] -= self.insertionRate[0]
-                    controlPointPos[0][1] -= self.insertionRate[1]
-                    controlPointPos[0][2] -= self.insertionRate[2]
+                    controlPointPos[self.posDoFsIdGrid] -= self.insertionDirection * self.insertionRate
 
         self.totalTime = self.totalTime + self.rootNode.findData('dt').value
         self.nbIterations = self.nbIterations + 1
 
-    def onAnimateEndEvent(self, event): # called at each end of animation step
+    def onAnimateEndEvent(self, event):  # called at each end of animation step
         pass
 
     def onKeypressedEvent(self, event):
         # Pressing A key activates/deactivates the automatic insertion (in the X axis direction)
-        if event['key']=='A':
-            if (self.autoInsertion):
+        if event['key'] == 'A':
+            if self.autoInsertion:
                 print("Automatic insertion stopped")
             else:
                 print("Automatic insertion started")
@@ -112,14 +145,78 @@ class InteractiveInsertionController(Sofa.Core.Controller):
 
         # Pressing R key inverse the insertion (switch between pushing and pulling)
         if event['key'] == 'C':
-            if (self.backwards):
+            if self.backwards:
                 print("Now pushing the catheter")
             else:
                 print("Now pulling the catheter")
             self.backwards = not self.backwards
 
+        if event['key'] == Key.uparrow:  # Up arrow
+            with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointPos[self.posDoFsIdGrid] += self.insertionDirection * self.insertionRate
+                if self.currentFrameId != 0:
+                    euclDist = controlPointPos[self.posDoFsIdGrid] - self.controlPointRefPos[self.posDoFsIdGridSingle]
+                    if np.linalg.norm(euclDist) > self.switchFrameDistance:
+                        # Moving the control point to the previous frame (as we are pushing)
+                        self.currentFrameId -= 1
+                        # Storing the new reference position by copy
+                        self.controlPointRefPos = np.array(self.mappedFramesNode.FramesMO.position.value[self.currentFrameId])
+                        # Updating the control point position and changing the frame to which the spring is attached
+                        with self.mappedFramesNode.FramesMO.position.writeable() as currentFramePos:
+                            controlPointPos[0] = currentFramePos[self.currentFrameId]
+                            with self.mappedFramesNode.controlSpring.points.writeable() as attachedFrame:
+                                attachedFrame[0] = self.currentFrameId
+                # else : we are pushing on the extremity frame, so nothing is to be changed
+
+        if event['key'] == Key.downarrow:  # Down arrow
+            with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointPos[self.posDoFsIdGrid] -= self.insertionDirection * self.insertionRate
+                if self.currentFrameId != self.nbFrames-1:
+                    euclDist = controlPointPos[self.posDoFsIdGrid] - self.controlPointRefPos[self.posDoFsIdGridSingle]
+                    if np.linalg.norm(euclDist) > self.switchFrameDistance:
+                        # Moving the control point to the following frame (as we are pulling)
+                        self.currentFrameId += 1
+                        # Storing the new reference position by copy
+                        self.controlPointRefPos = np.array(self.mappedFramesNode.FramesMO.position.value[self.currentFrameId])
+                        # Updating the control point position and changing the frame to which the spring is attached
+                        with self.mappedFramesNode.FramesMO.position.writeable() as currentFramePos:
+                            controlPointPos[0] = currentFramePos[self.currentFrameId]
+                            with self.mappedFramesNode.controlSpring.points.writeable() as attachedFrame:
+                                attachedFrame[0] = self.currentFrameId
+                # else : we are pulling on the extremity frame, so nothing is to be changed
+
+        if event['key'] == Key.leftarrow:  # Left arrow
+            # We apply a rotation of self.incrementAngle degrees around the insertion direction.
+            # We have to convert the quaternion part of the control point DoFs to a pyquaternion.Quaternion object,
+            # as the order of the quaternion coordinates in pyquaternion (w, x, y, z) is not the same as the Rigid3d
+            # objects in Sofa (x, y, z, w)
+            with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointSofaQuat = controlPointPos[self.quatDoFsIdGrid]  # np matrix of size 1x4
+                qx = controlPointSofaQuat[0][0]
+                qy = controlPointSofaQuat[0][1]
+                qz = controlPointSofaQuat[0][2]
+                qw = controlPointSofaQuat[0][3]
+                controlPointQuat = Quat(qw, qx, qy, qz)
+                newControlPointQuat = self.minusQuat * controlPointQuat
+                controlPointPos[self.quatDoFsIdGrid] = np.array([[newControlPointQuat[1], newControlPointQuat[2],
+                                                                newControlPointQuat[3], newControlPointQuat[0]]])
+
+        if event['key'] == Key.rightarrow:  # Right arrow
+            # Same as above, but with a quaternion corresponding to the opposite angle rotation
+            with self.controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointSofaQuat = controlPointPos[self.quatDoFsIdGrid]  # np matrix of size 1x4
+                qx = controlPointSofaQuat[0][0]
+                qy = controlPointSofaQuat[0][1]
+                qz = controlPointSofaQuat[0][2]
+                qw = controlPointSofaQuat[0][3]
+                controlPointQuat = Quat(qw, qx, qy, qz)
+                newControlPointQuat = self.plusQuat * controlPointQuat
+                controlPointPos[self.quatDoFsIdGrid] = np.array([[newControlPointQuat[1], newControlPointQuat[2],
+                                                                  newControlPointQuat[3], newControlPointQuat[0]]])
+
 
 class ColorMapController(Sofa.Core.Controller):
+
     def __init__(self, rootNode, springStiffness, *args, **kwargs):
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
 
@@ -157,11 +254,13 @@ class ColorMapController(Sofa.Core.Controller):
 
 class BendingController(Sofa.Core.Controller):
 
-    def __init__(self, rootNode, cosseratNode, bendingMoment, fixedIndices, momentAxis, *args, **kwargs):
+    def __init__(self, rootNode, cosseratNode, frameNode, bendingMoment, fixedIndices, momentAxis, *args, **kwargs):
         # These are needed (and the normal way to override from a python class)
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
 
         self.rootNode = rootNode
+        self.frameNode = frameNode
+        self.previousAttachedFrameId = [0]  # not used until bending is activated
         self.cosseratNode = cosseratNode  # rateAngularDeform
         self.bendingMoment = bendingMoment
         self.fixedIndices = fixedIndices
@@ -169,7 +268,8 @@ class BendingController(Sofa.Core.Controller):
         self.totalTime = 0.0
         self.nbIterations = 0
         self.bendingActivated = False
-        self.fixationActivated = True
+        self.fixationActivated = False
+        self.tipFixationActivated = False
 
     def onAnimateBeginEvent(self, event):  # called at each begin of animation step
 
@@ -184,26 +284,29 @@ class BendingController(Sofa.Core.Controller):
         if event['key'] == 'B':
             self.triggerBendingMoment()
         if event['key'] == 'F':
-            self.triggerFixation();
+            self.triggerFixation()
+        if event['key'] == 'T':
+            self.triggerTipFixation()
 
     def triggerBendingMoment(self):
         with self.cosseratNode.bendingMoment.forces.writeable() as moment:
-            if (self.bendingActivated):
+            if self.bendingActivated:
                 # Bending moment is currently activated, it should be deactivated
                 print("Bending moment deactivated")
                 for i in range(len(moment)):
                     moment[i] = [0.0, 0.0, 0.0]
                 self.bendingActivated = False
+
             else:
                 # self.bendingActivated = False => bending moment should be reactivated
                 print("Bending moment activated")
-                if (self.momentAxis == 'x'):
+                if self.momentAxis == 'x':
                     for i in range(len(moment)):
                         moment[i] = [self.bendingMoment, 0.0, 0.0]
-                elif (self.momentAxis == 'y'):
+                elif self.momentAxis == 'y':
                     for i in range(len(moment)):
                         moment[i] = [0.0, self.bendingMoment, 0.0]
-                elif (self.momentAxis == 'z'):
+                elif self.momentAxis == 'z':
                     for i in range(len(moment)):
                         moment[i] = [0.0, 0.0, self.bendingMoment]
                 else:
@@ -213,16 +316,41 @@ class BendingController(Sofa.Core.Controller):
                     moment[0] = [self.bendingMoment, 0.0, 0.0]
                 self.bendingActivated = True
 
+
     def triggerFixation(self):
-        # with self.cosseratNode.fixation.indices.value as fixedIndices:
-        fixedIndices = self.cosseratNode.fixation.indices.value
-        if (self.fixationActivated):
+
+        if self.fixationActivated:
             # Fixation is currently activated, it should be deactivated
             print("Fixation deactivated")
             self.cosseratNode.fixation.indices = []
             self.fixationActivated = False
+
         else:
-            # self.fixationActivated = False => fiwation should be reactivated
+            # self.fixationActivated = False => fixation should be reactivated
             print("Fixation activated")
             self.cosseratNode.fixation.indices = self.fixedIndices
             self.fixationActivated = True
+
+    def triggerTipFixation(self):
+
+        if self.tipFixationActivated:
+            # Tip fixation is currently activated, it should be deactivated
+            print("Tip fixation deactivated")
+            self.tipFixationActivated = False
+
+            # Moving the control point back to its original position, before bending
+            self.frameNode.controlSpring.points = self.previousAttachedFrameId
+            controlPointNode = self.rootNode.getChild('controlPointNode')
+            with controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointPos[0] = self.frameNode.FramesMO.position.value[self.previousAttachedFrameId[0]]
+        else:
+            # self.tipFixationActivated = False => tip fixation should be reactivated
+            print("Tip fixation activated")
+            self.tipFixationActivated = True
+
+            # Moving the control point to the beam base, in order to prevent motion during tip bending
+            self.previousAttachedFrameId = np.array(self.frameNode.controlSpring.points.value)  # copy
+            self.frameNode.controlSpring.points = [0]
+            controlPointNode = self.rootNode.getChild('controlPointNode')
+            with controlPointNode.controlPointMO.position.writeable() as controlPointPos:
+                controlPointPos[0] = self.frameNode.FramesMO.position.value[0]
