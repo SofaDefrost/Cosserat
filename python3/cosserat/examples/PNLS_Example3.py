99519232c2c81cee494e79f96a4e5ccbabf56835
yadagolo <yinoussa.adagolodjo@inria.fr>
2022-05-25 15:49:26 +0200

    adding a new example for testing the non linear strain of the algorithm

--- python3/cosserat/examples/PNLS_Example3.py
+++ python3/cosserat/examples/PNLS_Example3.py
@@ -54,26 +54,27 @@ class ForceController(Sofa.Core.Controller):
     def onAnimateEndEvent(self, event):
         if self.applyForce:
             if self.phase1:
-                if self.forceCoeff < pi:
+                if self.forceCoeff < 2*pi:
                     with self.controllerNode.position.writeable() as position:
-                        print(f' The forceCoeff is : {self.forceCoeff}')
-                        print(f' The control point is : {position[0]}')
-
-                        # orientation = Quat(position[0][3], position[0][4], position[0][5], position[0][6])  # get the orientation
-                        vec = Quat()  # get the orientation
-                        vec.rotateFromEuler([self.forceCoeff, 0., 0.])
+                        # vec = Quat()
+                        frames = self.frames.position[self.size]
+                        vec = Quat(frames[3], frames[4], frames[5], frames[6])  # get the orientation
+                        # print(f' The forceCoeff is : {self.forceCoeff}')
+                        print(f' The vec before : {vec}')
+                        # get the orientation
+                        vec.rotateFromEuler([0.01, 0., 0.])
                         vec.normalize()
-                        print(f' The control vec is : {vec}')
+                        print(f' The vec after : {vec}')
                         for i, v in enumerate(vec):
                             position[0][i+3] = v
-                        self.forceCoeff += 0.1
+                        self.forceCoeff += 0.01
                 else:
                     self.phase2 = True
 
             if self.phase2:
                 with self.controllerNode.position.writeable() as position:
                     if position[0][0] > 10.:
-                        position[0][0] -= 0.1
+                        position[0][0] -= 0.01
                     print(f' The new end effector is : {position[0]}')
 
 
@@ -97,11 +98,11 @@ def createScene(rootNode):
                                                    'hideBoundingCollisionModels hireForceFields '
                                                    'hideInteractionForceFields hideWireframe showMechanicalMappings')
     rootNode.findData('dt').value = 0.02
-    rootNode.findData('gravity').value = [0., -9.81, 0.]
-    # rootNode.findData('gravity').value = [0., 0., 0.]
+    # rootNode.findData('gravity').value = [0., -9.81, 0.]
+    rootNode.findData('gravity').value = [0., 0., 0.]
     rootNode.addObject('BackgroundSetting', color='1 1 1')
-    rootNode.addObject('FreeMotionAnimationLoop')
-    rootNode.addObject('GenericConstraintSolver', tolerance=1e-5, maxIterations=5e2)
+    # rootNode.addObject('FreeMotionAnimationLoop')
+    # rootNode.addObject('GenericConstraintSolver', tolerance=1e-5, maxIterations=5e2)
     # rootNode.addObject('Camera', position="-35 0 280", lookAt="0 0 0")
 
     solverNode = rootNode.addChild('solverNode')
@@ -124,10 +125,10 @@ def createScene(rootNode):
     # @todo attach the end effector of the beam with a control point
     EndEffectorColler = rootNode.addChild('EndEffectorController')
     controlMo = EndEffectorColler.addObject('MechanicalObject', template='Rigid3d', name="controlEndEffector",
-                            showObjectScale=0.2, position=[length, 0, 0, 0, 0, 0, 1])
+                            showObjectScale=0.3, position=[length, 0, 0, 0, 0, 0, 1], showObject=True)
 
     beamFrame.addObject('RestShapeSpringsForceField', name='spring',
-                        stiffness=1e10, angularStiffness=1e10, external_points=0,
+                        stiffness=1e8, angularStiffness=1e8, external_points=0,
                         external_rest_shape=controlMo.getLinkPath(), points=nbFrames, template="Rigid3d")
 
     cosseratNode = nonLinearCosserat.legendreControlPointsNode
