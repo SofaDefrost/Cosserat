ebbfd649648f3c6eb20a2ce1f221d9a4d4aea74c
Younes <yinoussa.adagolodjo@gmail.com>
2022-06-29 11:49:58 +0200

    some modification

--- python3/cosserat/__init__.py
+++ python3/cosserat/__init__.py
@@ -22,4 +22,8 @@ Contents of the library
 
 """
 
+<<<<<<< Updated upstream
 __all__ = ["cosseratObject", "nonLinearCosserat", "actuators", "scenes", "needle", "inverse", "usefulFunctions"]
+=======
+__all__ = ["cosseratObject", "actuators", "scenes", "needle", "inverse", "usefulFunctions"]
+>>>>>>> Stashed changes
