2b4cf05b65c793ec4c89d017a1ba5b43fd342826
Younes <yinoussa.adagolodjo@gmail.com>
2022-09-02 17:29:21 +0200

    update the scene

--- python3/cosserat/needle/needleInteractionTest.py
+++ python3/cosserat/needle/needleInteractionTest.py
@@ -21,7 +21,6 @@ from cosserat.createFemRegularGrid import createFemCubeWithParams
 from cosserat.usefulFunctions import pluginList
 from params import NeedleParameters
 
-
 params = NeedleParameters()
 
 needleGeometryConfig = {'init_pos': [0., 0., 0.], 'tot_length': params.Geometry.totalLength,
@@ -36,20 +35,55 @@ class Animation(Sofa.Core.Controller):
         self.rigidBaseMO = args[0]
         self.rateAngularDeformMO = args[1]
         self.contactListener = args[2]
+        self.generic = args[3]
+        self.entryPoint = []
+        self.threshold = 4.
+        self.needleCollisionModel = args[4]
+        self.constraintPoints = args[5]
+        self.inside = False
 
         self.rate = 0.2
         self.angularRate = 0.02
         return
 
+    def onAnimateEndEvent(self, event):
+        if self.contactListener.getContactPoints() and not self.inside:
+            vec = self.contactListener.getContactPoints()[0][1]
+            tip = [vec[0], vec[1], vec[2]]
+            # print(f' ====> The tip is : {tip}')
+            # print(f' ====> The force is : {self.generic.constraintForces[0]}')
+
+            if self.generic.constraintForces and self.generic.constraintForces[0] > self.threshold:
+                # @info 1. Save the entryPoint
+                self.entryPoint = tip
+                print(f' ====> The entryPoint is : {self.entryPoint}')
+                # @info 2. deactivate the contact constraint
+                # todo: add the code to deactivate the contact constraint
+                self.needleCollisionModel.findData('activated').value = 0
+                # @info 3. Add entryPoint point as the first sliding constraint
+                # todo: add the code according to #3
+                # with self.constraintPoints.position.writeable() as pos:
+                #     print(f'pos is : {dir(self.constraintPoints.position)}')
+                #     pos = self.entryPoint
+                self.inside = True
+
+            elif self.generic.constraintForces[0] > self.threshold:
+                print("Please activate computeConstraintForces data field inside the GenericConstraint component")
+        elif self.inside:
+            # 4. todo: add new constraint point inside the volume if needed.
+            # todo: This depends on the choice of algorithm
+            #  expl1: one can compare tip position related to the last constraint position inside the volume and
+            #  when this > than the constraintDistance we add new constraint point
+            # addNewConstraintPoint()
+
+            # 5. todo: If the user is pulling out the needle and the needle tip is behind is before the entryPoint,
+            # todo: activated the contact constraint.
+            # 5.1 self.inside=False
+            # 5.2 self.needleCollisionModel.findData('activated').value = 1
+            pass
+
     def onKeypressedEvent(self, event):
         key = event['key']
-        # ######## Rate angular #########
-        if key == "I":  # +
-            with self.rigidBaseMO.rest_position.writeable() as posA:
-                # posA[5][1] += self.angularRate
-                print(f' ====> contactListener 0 : {self.contactListener.getContactPoints()[0][1][0]}')
-                print(f' ====> contactListener 1 : {self.contactListener.getContactPoints()[0][1][1]}')
-
         if key == "K":  # -
             with self.rigidBaseMO.rest_position.writeable() as posA:
                 posA[5][1] -= self.angularRate
@@ -77,7 +111,7 @@ class Animation(Sofa.Core.Controller):
                     [0., -self.angularRate, 0.], 'ryxz')
                 qNew.normalize()
                 qNew.rotateFromQuat(qOld)
-                for i in range(0, 4):
+                for i in range(4):
                     posA[0][i + 3] = qNew[i]
 
         if ord(key) == 18:  # left
@@ -115,7 +149,6 @@ def createScene(rootNode):
                        contactDistance=params.contact.contactDistance, coneFactor=params.contact.coneFactor,
                        angleCone=0.1)
 
-
     rootNode.addObject('FreeMotionAnimationLoop')
     # rootNode.addObject('CollisionPipeline', verbose="0")
     # rootNode.addObject('BruteForceDetection', name="N2")
@@ -125,7 +158,8 @@ def createScene(rootNode):
     #                    contactDistance=params.contact.contactDistance, coneFactor=params.contact.coneFactor,
     #                    angleCone=params.contact.angleCone)
 
-    rootNode.addObject('GenericConstraintSolver', tolerance="1e-20", maxIterations="500", printLog="0")
+    generic = rootNode.addObject('GenericConstraintSolver', tolerance="1e-20", maxIterations="500",
+                                 computeConstraintForces=1, printLog="0")
 
     gravity = [0, 0, 0]
     rootNode.gravity.value = gravity
@@ -168,12 +202,13 @@ def createScene(rootNode):
     inputFEMCableMO = inputFEMCable.getLinkPath()
     outputPointMO = mappedPoints.getLinkPath()
 
-    conttactL = rootNode.addObject('ContactListener', name="contactListener", collisionModel1=cubeNode.gelNode.surfaceNode.surface.getLinkPath(),
+    conttactL = rootNode.addObject('ContactListener', name="contactListener",
+                                   collisionModel1=cubeNode.gelNode.surfaceNode.surface.getLinkPath(),
                                    collisionModel2=needleCollisionModel.pointColli.getLinkPath())
-    rootNode.addObject(Animation(needle.rigidBaseNode.RigidBaseMO, needle.cosseratCoordinateNode.cosseratCoordinateMO, conttactL))
+    rootNode.addObject(Animation(needle.rigidBaseNode.RigidBaseMO, needle.cosseratCoordinateNode.cosseratCoordinateMO,
+                                 conttactL, generic, needleCollisionModel,inputFEMCable))
     mappedPointsNode.addObject(
         'CosseratNeedleSlidingConstraint', name="QPConstraint")
     mappedPointsNode.addObject('DifferenceMultiMapping', name="pointsMulti", input1=inputFEMCableMO, lastPointIsFixed=0,
                                input2=inputCableMO, output=outputPointMO, direction="@../../FramesMO.position")
     return rootNode
-
