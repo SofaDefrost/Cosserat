e6eb6872542e59cd1ec1ca62ebcac43cc5970f83
Camille Krewcun <camille.krewcun@inria.fr>
2023-02-01 09:46:26 +0100

    [CoaxialModel] Update test scene
    New option for RigidDistanceMapping, allowing the difference computation
    to be made locally (instead of in the global frame). Not used for the
    moment

--- python3/cosserat/plasticity/testScene_coaxialBeamModel_two_instruments_coaxial_constraint_version.py
+++ python3/cosserat/plasticity/testScene_coaxialBeamModel_two_instruments_coaxial_constraint_version.py
@@ -502,7 +502,8 @@ def createScene(rootNode):
                                   input1=coaxialFramesMO1.getLinkPath(),
                                   input2=coaxialFramesMO0.getLinkPath(),
                                   output=rigidDiffMO.getLinkPath(),
-                                  first_point=[], second_point=[])
+                                  first_point=[], second_point=[],
+                                  newVersionOfFrameComputation=False)
 
     constraintWith0Node.addObject('CosseratNeedleSlidingConstraint',
                                   name='constraintMappingConstraint',
