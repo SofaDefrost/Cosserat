3803dd259543d9332d57a59ad6f20a8fd4766561
Hugo Talbot <hugo.talbot@sofa-framework.org>
2023-06-26 11:10:41 +0200

    add root init py

---
+++ examples/python3/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-longeurS
+"""
+Template for needle.
+
+Content:
+********
+    .. 	scenes related to the use of cosserat model needle in a direct simulation
+    .. autosummary::
+    :toctree: _autosummary
+
+    cosserat.needle.params
+
+
+"""
+
+__all__ = ["cosserat"]
