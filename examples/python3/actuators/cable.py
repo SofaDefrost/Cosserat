6efb5b63d5b7536ea2a3be67313ceb4ea583f8fb
Yinoussa Adagolodjo <yinoussa.adagolodjo2@univ-lille.fr>
2024-03-01 08:58:39 +0100

    update files and fix some bugs
    Update files and fix some bugs related to sofa version

--- examples/python3/actuators/cable.py
+++ examples/python3/actuators/cable.py
@@ -1,13 +1,15 @@
-def PullingCable(attachedTo=None,
-                 name="Cable",
-                 cableGeometry=[[1.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
-                 rotation=[0.0, 0.0, 0.0],
-                 translation=[0.0, 0.0, 0.0],
-                 uniformScale=1.0,
-                 pullPointLocation=None,
-                 initialValue=0.0,
-                 valueType="displacement",
-                 input=None):
+def PullingCable(
+    attachedTo=None,
+    name="Cable",
+    cableGeometry=[[1.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
+    rotation=[0.0, 0.0, 0.0],
+    translation=[0.0, 0.0, 0.0],
+    uniformScale=1.0,
+    pullPointLocation=None,
+    initialValue=0.0,
+    valueType="displacement",
+    input=None,
+):
     """Adds a cable constraint.
 
     The constraint apply to a parent mesh.
@@ -42,32 +44,44 @@ def PullingCable(attachedTo=None,
 
     """
     #  This add a new node in the scene. This node is appended to the finger's node.
-    cable = attachedTo.addChild(name)
+    if attachedTo is not None:
+        cable = attachedTo.addChild(name)
+    else:
+        print("Attach node to the pulling cable!")
+        return
 
     # This add a MechanicalObject, a component holding the degree of freedom of our
     # mechanical modelling. In the case of a cable it is a set of positions specifying
     # the points where the cable is passing by.
-    cable.addObject('MechanicalObject', position=cableGeometry, showIndices="1",
-                    rotation=rotation, translation=translation, scale=uniformScale)
+    cable.addObject(
+        "MechanicalObject",
+        position=cableGeometry,
+        showIndices="1",
+        rotation=rotation,
+        translation=translation,
+        scale=uniformScale,
+    )
 
     # Add a CableConstraint object with a name.
     # the indices are referring to the MechanicalObject's positions.
     # The last indice is where the pullPoint is connected.
     if pullPointLocation is not None:
-        cable.addObject('CableConstraint',
-                        indices=list(range(len(cableGeometry))),
-                        pullPoint=pullPointLocation,
-                        value=initialValue,
-                        valueType=valueType,
-                        hasPullPoint=True
-                        )
+        cable.addObject(
+            "CableConstraint",
+            indices=list(range(len(cableGeometry))),
+            pullPoint=pullPointLocation,
+            value=initialValue,
+            valueType=valueType,
+            hasPullPoint=True,
+        )
     else:
-        cable.addObject('CableConstraint',
-                        indices=list(range(len(cableGeometry))),
-                        value=initialValue,
-                        valueType=valueType,
-                        hasPullPoint=False
-                        )
+        cable.addObject(
+            "CableConstraint",
+            indices=list(range(len(cableGeometry))),
+            value=initialValue,
+            valueType=valueType,
+            hasPullPoint=False,
+        )
 
     # This add a BarycentricMapping. A BarycentricMapping is a key element as it will add a bi-directional link
     # between the cable's DoFs and the parents's ones so that movements of the cable's DoFs will be mapped
@@ -75,7 +89,7 @@ def PullingCable(attachedTo=None,
     # cable.addObject('BarycentricMapping', name="Mapping", mapForces=False, mapMasses=False,
     #                 input=input)
     # cable.addObject('RigidMapping', name="Mapping")
-    cable.addObject('IdentityMapping', name="Mapping")
+    cable.addObject("IdentityMapping", name="Mapping")
 
     return cable
 
@@ -85,8 +99,8 @@ def createScene(node):
     from stlib3.physics.deformable import ElasticMaterialObject
 
     MainHeader(node, plugins=["SoftRobots"])
-    target = ElasticMaterialObject(volumeMeshFileName="mesh/liver.msh",
-                                   totalMass=0.5,
-                                   attachedTo=node)
+    target = ElasticMaterialObject(
+        volumeMeshFileName="mesh/liver.msh", totalMass=0.5, attachedTo=node
+    )
 
     PullingCable(target)
