a79ee6f033e7b947635cb6721c2b184f400306a6
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-07-13 00:52:15 +0200

    Refactor PointManager
    It looks like it is a controller... (but in that cas why exposing it to python ?)
    Further refactoring should be added.

--- src/Cosserat/engine/PointsManager.cpp
+++ src/Cosserat/engine/PointsManager.cpp
@@ -1,19 +1,10 @@
 
-#include "PointsManager.inl"
+#include <Cosserat/engine/PointsManager.inl>
 #include <sofa/core/ObjectFactory.h>
-#include <sofa/core/visual/VisualParams.h>
-#include <math.h>
-#include <assert.h> /* assert */
 
-namespace sofa::core::behavior
+namespace cosserat::controller
 {
-
-    using namespace sofa::defaulttype;
-
-    SOFA_DECL_CLASS(PointsManager)
-
-    int PointsManagerClass = core::RegisterObject("add and remove "
-                                                  "points from the state will taking into account the changes inside the modifier and the container")
-                                .add<PointsManager>();
-
-} // namespace sofa
+int PointsManagerClass = sofa::core::RegisterObject(
+                             R"doc(Add or remove controls point in a beam structure)doc")
+                         .add<PointsManager>();
+} // namespace cosserat::controller
