2ac7d717ce35c8bbfbc400df4d96dcf81376285c
Appa <appasalifbaba.sonko.etu@univ-lille.fr>
2026-05-20 17:27:39 +0200

    Petites modifs dans la scène de test

--- tutorials/getting_started/using_lie_algebra/test2Frames2Strain.py
+++ tutorials/getting_started/using_lie_algebra/test2Frames2Strain.py
@@ -3,16 +3,7 @@
 Test de la fonction applyJ de Frames2StrainCosseratMapping
 """
 
-import os
-import sys
-
-# Add the python package to the path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "..", "python"))
-
-from cosserat import BeamGeometryParameters, CosseratGeometry
-
-from introduction_and_setup import (_add_cosserat_frame, _add_cosserat_state,
-                                    _add_rigid_base, add_mini_header)
+from introduction_and_setup import add_mini_header
 
 v_damping_param: float =  8e-1  # Damping parameter for dynamics
 beam_length :float = 15.0
@@ -26,7 +17,9 @@ def createScene(root_node):
     add_mini_header(root_node)
     root_node.addObject('RequiredPlugin', pluginName='Sofa.Component.LinearSolver.Direct') # Needed to use components [SparseLDLSolver]  
     root_node.addObject('RequiredPlugin', pluginName='Sofa.Component.ODESolver.Backward') # Needed to use components [EulerImplicitSolver]  
-
+    root_node.addObject('RequiredPlugin', name='Sofa.Component.LinearSolver.Iterative') # Needed to use components [CGLinearSolver]  
+    root_node.addObject('RequiredPlugin', name='Sofa.Component.MechanicalLoad') # Needed to use components [ConstantForceField]  
+   
     # Add gravity
     root_node.gravity = [0, -9.81, 0]  # Add gravity!
 
@@ -40,65 +33,33 @@ def createScene(root_node):
         rayleighMass="0.0",
         vdamping=v_damping_param,  # Damping parameter for dynamics
     )
-    solver_node.addObject("SparseLDLSolver", template="CompressedRowSparseMatrixMat3x3d", name="solver")
-
-    # === NEW APPROACH: Use CosseratGeometry with more sections for smoother dynamics ===
-    beam_geometry_params = BeamGeometryParameters(
-        beam_length=beam_length,
-        nb_section=nb_section,
-        nb_frames=nb_section,
-    )
-
-    # Create geometry object
-    beam_geometry = CosseratGeometry(beam_geometry_params)
+    solver_node.addObject("CGLinearSolver", iterations=25, tolerance=1e-7, threshold=1e-7)
 
     # Create rigid base
-    base_node = _add_rigid_base(solver_node)
-    # Create bending states with a curve (last section has more bending)
-    # We start with a slightly bent beam to better visualize the effect of gravity.
-    # custom_bending_states = []
-    # for i in range(beam_geometry.get_number_of_sections()):
-    #     custom_bending_states.append([0, 0.0, 0.1])
-
-    # # Create cosserat state using geometry
-    # bending_node = _add_cosserat_state(solver_node, beam_geometry, node_name="cosserat_states",
-    #                                    custom_bending_states=custom_bending_states)
-
-    # # Create cosserat frame with mass (important for dynamics!)
-    # # The mass is distributed uniformly along the beam. Without mass, the beam
-    # # would not be affected by gravity.
-    # frame_node = _add_cosserat_frame(
-    #     base_node, bending_node, beam_geometry, beam_mass=5.0
-    # )
-
-    ### Tige 2: (same base_node)
-    section_curv_abs = [i*section_length for i in range(nb_section+1)]
-    frame_curv_abs = section_curv_abs
-
-    frame_positions = [[0., 0., 0., 0., 0., 0., 1.],
-                       [4.79426, 1.22417, 0., 0., 0., 0.247404, 0.968912],
-                       [8.41471, 4.59698, 0., 0., 0., 0.479426, 0.877583],
-                       [9.97495, 9.29263, 0., 0., 0., 0.681639, 0.731689]]
+    base_node = solver_node.addChild("rigid_base")
+    base_node.addObject("MechanicalObject", template="Rigid3d", name="cosserat_base_mo", 
+                        position=[0., 0., 0., 0., 0., 0., 1.], showIndices="1", showObject="1", showObjectScale=0.1)
+    base_node.addObject("RestShapeSpringsForceField", name="spring", stiffness=stiffness_param, angularStiffness=stiffness_param,
+                         external_points="0", mstate="@cosserat_base_mo", points="0", template="Rigid3d")
     
+    # frame_positions = [[i*section_length, 0., 0., 0., 0., 0., 1.] for i in range(nb_section+1)]
+    frame_positions = [[0., 0., 0., 0., 0., 0., 1.],
+                    [4.79426, 1.22417, 0., 0., 0., 0.247404, 0.968912],
+                    [8.41471, 4.59698, 0., 0., 0., 0.479426, 0.877583],
+                    [9.97495, 9.29263, 0., 0., 0., 0.681639, 0.731689]]    
 
-    frame_node2 = solver_node.addChild("frame_node")
-    base_node.addChild(frame_node2)
-
-    frames_mo2 = frame_node2.addObject("MechanicalObject", template="Rigid3d",
+    frame_node = solver_node.addChild("frame_node")
+    frames_mo = frame_node.addObject("MechanicalObject", template="Rigid3d",
                                     name="FramesMO", position=frame_positions, 
                                     showIndices="1", showObject="1", showObjectScale=0.8)
+    frame_node.addObject("UniformMass", totalMass=5.0)
 
-    frame_node2.addObject("RestShapeSpringsForceField", stiffness=stiffness_param, angularStiffness=stiffness_param,
-                         external_points="0", points="0", template="Rigid3d")
-    
-    frame_node2.addObject("UniformMass", totalMass=5.0)
-    ##Strain (sortie du mapping)
-    #*******strain state**********
-    strain_node = frame_node2.addChild("strain_node")
-    # frame_node2.addChild(strain_node)
-    
-    strain_node.addObject("MechanicalObject", template="Vec3d", name="strain_state")
-    
+    # frame_node.addObject("ConstantForceField", forces=[[0, 9.81, 0, 0, 0, 0]] * (nb_section+1))
+
+    strain_node = base_node.addChild("strain_node")
+    frame_node.addChild(strain_node)
+    strain_node.addObject("MechanicalObject", template="Vec3d", name="strain_state", position=[[0., 0., 0.1] for _ in range(nb_section)])
+        
     strain_node.addObject("BeamHookeLawForceField",
                            crossSectionShape="circular", 
                            length=[section_length]*nb_section, 
@@ -106,9 +67,11 @@ def createScene(root_node):
                            youngModulus=1.0e4, 
                            poissonRatio=0.38)
     
+    section_curv_abs = [i*section_length for i in range(nb_section+1)]
+    frame_curv_abs = section_curv_abs
     strain_node.addObject("Frames2StrainCosseratMapping", curv_abs_input=section_curv_abs, 
-                        curv_abs_output=frame_curv_abs, name="cosseratMapping2", 
-                        input1=frames_mo2.getLinkPath(), 
+                        curv_abs_output=frame_curv_abs, name="cosseratMapping", 
+                        input1=frames_mo.getLinkPath(), 
                         input2=base_node.cosserat_base_mo.getLinkPath(), 
                         output=strain_node.strain_state.getLinkPath(), 
                         debug=0,
@@ -117,3 +80,10 @@ def createScene(root_node):
  
 
     return root_node
+
+
+
+# frame_positions = [[0., 0., 0., 0., 0., 0., 1.],
+#                    [4.79426, 1.22417, 0., 0., 0., 0.247404, 0.968912],
+#                    [8.41471, 4.59698, 0., 0., 0., 0.479426, 0.877583],
+#                    [9.97495, 9.29263, 0., 0., 0., 0.681639, 0.731689]]
